01ca4fe355cb475e41b8ba59b1d5c85a9eaec77c
Theophile Avoyne <tavoyne@gmail.com>
2020-11-23 11:43:16 +0100

    Added more lights

--- src/three/createLight.js
+++ src/three/createLight.js
@@ -1,11 +1,22 @@
-import { PointLight } from 'three';
+import { Group, PointLight } from 'three';
 
 const color = 0xffffff;
-const position = [0, 0, 100];
+const positions = [
+  [0, 0, 100],
+  [100, 0, 0],
+  [0, 0, -100],
+  [-100, 0, -100],
+  [0, -100, 0], // under
+];
 
 export default () => {
-  const light = new PointLight(color);
-  light.position.set(...position);
+  const lights = new Group();
 
-  return light;
+  positions.forEach((position) => {
+    const light = new PointLight(color);
+    light.position.set(...position);
+    lights.add(light);
+  });
+
+  return lights;
 };
